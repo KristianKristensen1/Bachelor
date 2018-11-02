b2183df750341c82869e0dfe76590de299d96a61
pandagud <pandagud@gmail.com>
2018-11-02 12:44:51 +0100

    Sorry! der var lige en lille fejl med interfacet.

--- FrontEndBA/Utility/EmailHelper/EmailToParticipantHelper.cs
+++ FrontEndBA/Utility/EmailHelper/EmailToParticipantHelper.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MimeKit;
+using MailKit;
+using MailKit.Net.Smtp;
 
 namespace FrontEndBA.Utility.EmailHelper
 {
     public class EmailToParticipantHelper
     {
+        // https://www.youtube.com/watch?v=C4O8vqg295o
+
         public void SendMessge()
         {
             var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Tandlaehoejskolen", "donotreplyTandlægeHøjskolen@gmail.com"));
+
+
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, false);
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
             
 
             
