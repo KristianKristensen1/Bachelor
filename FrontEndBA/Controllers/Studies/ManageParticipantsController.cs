2c0acd6a415c32a1520600c23bbec71c866d9642
Kristian Kristensen <kristian-kristensen@hotmail.com>
2018-11-05 12:25:54 +0100

    Manage participant stuff

---
+++ FrontEndBA/Controllers/Studies/ManageParticipantsController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FrontEndBA.Models;
+using FrontEndBA.Utility;
+using Microsoft.AspNetCore.Mvc;
+using BachelorBackEnd;
+
+namespace FrontEndBA.Controllers.Studies
+{
+    public class ManageParticipantsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ManageParticipants(int studyID, string studyName)
+        {
+            ManageParticipantsHelper manageParticipantsHelper = new ManageParticipantsHelper();
+            ManageParticipantModel manageParticipantModel = new ManageParticipantModel();
+            manageParticipantModel = manageParticipantsHelper.CreateManageParticipantModel(studyID, studyName);
+
+            return View(manageParticipantModel);
+        }
+
+
+        public ActionResult AddParticipant(ManageParticipantModel mpModel)
+        {
+            ManageStudyHandler msh = new ManageStudyHandler(new bachelordbContext());
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ManageParticipantStatus manageParticipantStatus = msh.AddParticipantToStudyDB(mpModel.participantID, mpModel.studyID);
+                    if (manageParticipantStatus.success)
+                    {
+                        return RedirectToAction("ManageParticipants", "ManageParticipants", new { studyID = mpModel.studyID, studyName = mpModel.participantID });
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("participantID", manageParticipantStatus.errormessage);
+                    }
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            mpModel.participants = msh.getParticipantsDB(mpModel.studyID);
+            return View("ManageParticipants", mpModel);
+        }
+
+        public ActionResult RemoveParticipant(ManageParticipantModel mpModel)
+        {
+            ManageStudyHandler msh = new ManageStudyHandler(new bachelordbContext());
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ManageParticipantStatus manageParticipantStatus = msh.RemoveParticipantFromStudyDB(mpModel.participantID, mpModel.studyID);
+                    if (manageParticipantStatus.success)
+                    {
+                        return RedirectToAction("ManageParticipants", "ManageParticipants", new { studyID = mpModel.studyID, studyName = mpModel.participantID });
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("ParticipantID", manageParticipantStatus.errormessage);
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            mpModel.participants = msh.getParticipantsDB(mpModel.studyID);
+            return View("ManageParticipants", mpModel);
+        }
+    }
+}
--- StudyManagementSystem/DAOImplementations/ManageStudyHandler.cs
+++ StudyManagementSystem/DAOImplementations/ManageStudyHandler.cs
@@ -170,5 +170,76 @@ namespace BachelorBackEnd
             Researcher researcher = _context.Researcher.FirstOrDefault(res => res.IdResearcher == id);
             return researcher;
         }
+
+        public List<Participant> getParticipantsDB(int studyID)
+        {
+            List<Participant> participants = new List<Participant>();
+
+            List<int> PartIDs = _context.Studyparticipant.Where(x => x.IdStudy == studyID).Select(partID => partID.IdParticipant).ToList();
+
+            foreach (var id in PartIDs)
+            {
+                participants.Add(_context.Participant.FirstOrDefault(part => part.IdParticipant == id));
+
+            }
+            return participants;
+
+        }
+
+        public ManageParticipantStatus AddParticipantToStudyDB(int partID, int studyID)
+        {
+            ManageParticipantStatus manageParticipantStatus = new ManageParticipantStatus();
+            Studyparticipant studpart = _context.Studyparticipant.FirstOrDefault(x => x.IdStudy == studyID && x.IdParticipant == partID);
+            if(studpart == null)
+            {
+                Participant participant = _context.Participant.FirstOrDefault(part => part.IdParticipant == partID);
+                if (participant != null)
+                {
+                    //Participant exists, but is not enrolled.
+                    studpart = new Studyparticipant();
+                    studpart.IdParticipant = partID;
+                    studpart.IdStudy = studyID;
+                    _context.Studyparticipant.Add(studpart);
+                    _context.SaveChanges();
+                    manageParticipantStatus.success = true;
+                }
+                else
+                {
+                    //Participant with this ID does not exists. 
+                    manageParticipantStatus.success = false;
+                    manageParticipantStatus.errormessage = "Participant with this ID does not exist in the system";
+                }
+
+
+            }
+            else
+            {
+                //Participant allready enrolled in study
+                manageParticipantStatus.success = false;
+                manageParticipantStatus.errormessage = "Participant is all ready enrolled in study";
+            }
+            return manageParticipantStatus;
+        }
+
+        public ManageParticipantStatus RemoveParticipantFromStudyDB(int partID, int studyID)
+        {
+            ManageParticipantStatus manageParticipantStatus = new ManageParticipantStatus();
+            Studyparticipant studPart = _context.Studyparticipant.FirstOrDefault(x => x.IdParticipant == partID && x.IdStudy == studyID);
+            if (studPart != null)
+            {
+                //Participant is enrolled in study
+                _context.Studyparticipant.Remove(studPart);
+                _context.SaveChanges();
+                manageParticipantStatus.success = true;
+            }
+            else
+            {
+                //Participant is not enrolled in study
+                manageParticipantStatus.errormessage = "Participant is not enrolled in study";
+                manageParticipantStatus.success = false;
+            }
+            return manageParticipantStatus;
+
+        }
     }
 }
