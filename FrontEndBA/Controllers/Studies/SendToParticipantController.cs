eab38ed738fcbf116a8f0ccbc26adc39913a05ab
pandagud <pandagud@gmail.com>
2018-11-05 10:12:18 +0100

    Changes to Email story

--- FrontEndBA/Controllers/Studies/SendToParticipantController.cs
+++ FrontEndBA/Controllers/Studies/SendToParticipantController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using BachelorBackEnd;
 using FrontEndBA.Models.CreateStudy;
+using FrontEndBA.Models.ResearcherModel.EmailModels;
 using Microsoft.AspNetCore.Mvc;
 using FrontEndBA.Utility;
+using FrontEndBA.Utility.EmailHelper;
 
 namespace FrontEndBA.Controllers.Studies
 {
@@ -13,14 +17,41 @@ namespace FrontEndBA.Controllers.Studies
         public IActionResult Index(int studyID)
         {
             ViewStudyModelHelper viewStudyModelHelper = new ViewStudyModelHelper();
-           
-            return View(viewStudyModelHelper.createViewStudyModel(studyID));
+           SendingModel sendToParticipantModel = new SendingModel();
+            sendToParticipantModel.studies = viewStudyModelHelper.createViewStudyModel(studyID);
+            return View(sendToParticipantModel);
         }
 
-        public IActionResult Create()
+        public IActionResult Create(SendingModel sModel)
         {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //Gets the id from JWT. The id is used to retrieve user from database. 
+                    var identity = (ClaimsIdentity)User.Identity;
+                    IEnumerable<Claim> claims = identity.Claims;
+                    int id_researcher = Convert.ToInt32(claims.ElementAt(3).Value);
 
-            return View();
+                    
+                    ManageStudyHandler msh  = new ManageStudyHandler(new bachelordbContext());
+                    //msh.
+
+                    // Convert to create the right format
+                    EmailToParticipantHelper emailHelper = new EmailToParticipantHelper();
+                    emailHelper.SendMessge(sModel);
+
+
+
+                }
+                catch (Exception e)
+                {
+                   
+                    ////cshelper.ErrorHandle(curCriteria,cs,curStudy
+                    return View("Index");
+                }
+            }
+            return View("index");
         }
     }
 }
